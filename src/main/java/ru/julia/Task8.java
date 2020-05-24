1af13ab3d540ed40531fa7a493c77543c592f390
julia <89081314528@mail.ru>
2020-05-24 22:13:38 +0300

    24/05 3

--- src/main/java/ru/julia/Task8.java
+++ src/main/java/ru/julia/Task8.java
@@ -1,19 +1,69 @@
 package ru.julia;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * 3) есть текст, нужно заменить слова на конкретные - всех птиц заменить на берд, животных на энимал
  */
 public class Task8 {
     public static void main(String[] args) {
+        String text = "Птички- синички уснули в саду, коровки дают молоко, лошадки говорят игого";
+        String newText = deletePunctuationMarks(text);
+        System.out.println(newText);
+        List<String> list = listOfWords(newText);
+        System.out.println(list);
+        Map<String, String> map = mapFromList(list);
+        List<String> newList = listFromMap(map);
+        System.out.println(newList);
+        List<String> newList2 = listWithChanges(list);
+        System.out.println(newList2);
 
     }
-    public static String deletePunctuationMarks2(String text) {
-        return text.replaceAll("\\.", "").replaceAll(",","").
-                replaceAll("!", "").replaceAll("\\?", "");
+
+    public static List<String> listFromMap(Map<String, String> map) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < map.size(); i++) {
+            list.add(map.get(i));
+        }
+        return list;
     }
-    public static List<String> listOfWords2(String newStr) {
-        return Arrays.asList(newStr.split(" "));}
+
+    public static String deletePunctuationMarks(String text) {
+        return text.replaceAll("\\.", "").replaceAll(",", "").
+                replaceAll("!", "").replaceAll("\\?", "").replaceAll("\\-", "");
+    }
+
+    public static List<String> listOfWords(String newStr) {
+        return Arrays.asList(newStr.split(" "));
+    }
+
+    public static Map<String, String> mapFromList(List<String> strings) {
+        Map<String, String> mapFromList = new HashMap<String, String>();
+        for (int i = 0; i < strings.size(); ++i) {
+            String key = strings.get(i);
+            if (key.equals("синички")) {
+                mapFromList.put("bird", "bird");
+            } else if (key.equals("коровки") || key.equals("лошадки")) {
+                mapFromList.put("animal", "animal");
+            } else {
+                mapFromList.put(key, key);
+            }
+        }
+        return mapFromList;
+    }
+
+    public static List<String> listWithChanges(List<String> list) {
+        List<String> newList = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals("синички")) {
+                newList.add("bird");
+            } else if (list.get(i).equals("коровки") || list.get(i).equals("лошадки")) {
+                newList.add("animal");
+            } else {
+                newList.add(list.get(i));
+            }
+        }
+        return newList;
+    }
 }
+
