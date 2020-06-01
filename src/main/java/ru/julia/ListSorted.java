ee1cbb1040f525ea90b1f7efd8be28600a15c6fc
julia <89081314528@mail.ru>
2020-06-01 21:29:51 +0300

    01/06

---
+++ src/main/java/ru/julia/ListSorted.java
@@ -0,0 +1,30 @@
+package ru.julia;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * метод, который принимает лист и возвращает, отсортировано он по возрастанию, или нет
+ */
+public class ListSorted {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(3);
+        list.add(5);
+        list.add(6);
+        list.add(9);
+        list.add(4);
+        System.out.println(sorted(list));
+    }
+    public static boolean sorted(List<Integer> list) {
+        Integer max = list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            if(list.get(i) > max) {
+                max = list.get(i);
+            } else
+            return false;
+        }
+        return true;
+    }
+}
